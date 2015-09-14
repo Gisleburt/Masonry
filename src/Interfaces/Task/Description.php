a2b61829b653b93f16e4eaf9e201da43f94d31e4
Daniel Mason <daniel.mason@thefoundry.co.uk>
2015-09-14 16:49:52 +0000

    ToDo: Work this out. I think the description class name should become the capability string

--- src/Interfaces/Task/Description.php
+++ src/Interfaces/Task/Description.php
@@ -13,7 +13,11 @@
 
 namespace Foundry\Masonry\Interfaces\Task;
 
-
+/**
+ * Interface Description
+ * ToDo: Formalise this as I have no idea how it should work
+ * @package Foundry\Masonry\Interfaces\Task
+ */
 interface Description
 {
 
