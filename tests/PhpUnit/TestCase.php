39e73c47d2a3cc2656eea9b432cc5824d4ad5938
Daniel Mason <daniel.mason@thefoundry.co.uk>
2015-09-24 16:06:38 +0000

    Preparing for Unit Tests

---
+++ tests/PhpUnit/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * TestCase.php
+ * PHP version 5.4
+ * 2015-09-14
+ *
+ * @package   Masonry
+ * @category
+ * @author    Daniel Mason <daniel.mason@thefoundry.co.uk>
+ * @copyright 2015 The Foundry Visionmongers
+ */
+
+
+namespace Foundry\Masonry\Tests\PhpUnit;
+
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
