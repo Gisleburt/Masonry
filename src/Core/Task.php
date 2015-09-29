587268d116873e446fdbfd9772954438904c6f3f
Daniel Mason <daniel.mason@thefoundry.co.uk>
2015-09-29 10:16:40 +0000

    Task + Tests

---
+++ src/Core/Task.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * TaskInterfaceInterface.php
+ * PHP version 5.4
+ * 2015-09-04
+ *
+ * @package   Masonry
+ * @category
+ * @author    Daniel Mason <daniel.mason@thefoundry.co.uk>
+ * @copyright 2015 The Foundry Visionmongers
+ */
+
+
+namespace Foundry\Masonry\Core;
+
+use Foundry\Masonry\Core\Task\History;
+use Foundry\Masonry\Core\Task\Status;
+use Foundry\Masonry\Interfaces\Task\DescriptionInterface;
+use Foundry\Masonry\Interfaces\Task\History\ResultInterface;
+use Foundry\Masonry\Interfaces\Task\HistoryInterface;
+use Foundry\Masonry\Interfaces\Task\StatusInterface;
+use Foundry\Masonry\Interfaces\Task\History\ReasonInterface;
+use Foundry\Masonry\Interfaces\TaskInterface;
+
+class Task implements TaskInterface
+{
+    /**
+     * @var DescriptionInterface
+     */
+    private $description;
+
+    /**
+     * @var History
+     */
+    private $history;
+
+    /**
+     * Construct a task, give it a description
+     * @param DescriptionInterface $description
+     */
+    public function __construct(DescriptionInterface $description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Returns the data for the worker to use.
+     * This data is generally bespoke.
+     * @return DescriptionInterface
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * The current status of this task
+     * @return StatusInterface
+     */
+    public function getStatus()
+    {
+        if(!$this->getHistory()->getEvents()) {
+            return new Status(Status::STATUS_NEW);
+        }
+        if(!$this->getHistory()->getLastEvent()->getEndTime()) {
+            return new Status(Status::STATUS_IN_PROGRESS);
+        }
+        if($this->getHistory()->getLastEvent()->getResult() == ResultInterface::RESULT_INCOMPLETE) {
+            return new Status(Status::STATUS_DEFERRED);
+        }
+        return new Status(Status::STATUS_COMPLETE);
+    }
+
+    /**
+     * The history of this task
+     * @return HistoryInterface
+     */
+    public function getHistory()
+    {
+        if(!$this->history) {
+            $this->history = new History();
+        }
+        return $this->history;
+    }
+
+    /**
+     * Notify the task that work has begun
+     * @return $this
+     */
+    public function start()
+    {
+        $this->getHistory()->addEvent(
+            new History\Event()
+        );
+    }
+
+    /**
+     * Complete the task
+     * @param ResultInterface|null $result Defaults to succeeded
+     * @param ReasonInterface|null $reason Defaults to empty
+     * @return $this
+     */
+    public function complete(ResultInterface $result = null, ReasonInterface $reason = null)
+    {
+        if(!$result) {
+            $result = new History\Result(History\Result::RESULT_SUCCEEDED);
+        }
+        $this->getHistory()->getLastEvent()->endEvent($result, $reason);
+        return $this;
+    }
+
+    /**
+     * Cancel the task (might be able to do it later)
+     * @param ReasonInterface $reason
+     * @return $this
+     */
+    public function cancel(ReasonInterface $reason = null)
+    {
+        $this->getHistory()->getLastEvent()->endEvent(
+            new History\Result(History\Result::RESULT_INCOMPLETE),
+            $reason
+        );
+    }
+}
---
+++ tests/PhpUnit/Core/TaskTest.php
@@ -0,0 +1,269 @@
+<?php
+/**
+ * TaskInterfaceInterface.php
+ * PHP version 5.4
+ * 2015-09-04
+ *
+ * @package   Masonry
+ * @category
+ * @author    Daniel Mason <daniel.mason@thefoundry.co.uk>
+ * @copyright 2015 The Foundry Visionmongers
+ */
+
+
+namespace Foundry\Masonry\Tests\PhpUnit\Core;
+
+use Foundry\Masonry\Core\Task;
+use Foundry\Masonry\Interfaces\Task\StatusInterface;
+use Foundry\Masonry\Tests\PhpUnit\TestCase;
+
+/**
+ * Class TaskTest
+ *
+ * @package Foundry\Masonry
+ * @coversDefaultClass \Foundry\Masonry\Core\Task
+ */
+class TaskTest extends TestCase
+{
+    /**
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Foundry\Masonry\Interfaces\Task\DescriptionInterface
+     */
+    protected function getDescription()
+    {
+        return $this->getMockBuilder('\Foundry\Masonry\Interfaces\Task\DescriptionInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @test
+     * @covers ::__construct
+     * @return void
+     */
+    public function testConstruct()
+    {
+        $description = $this->getDescription();
+        $task = new Task($description);
+
+        $this->assertSame(
+            $description,
+            $this->getObjectAttribute($task, 'description')
+        );
+    }
+
+    /**
+     * @test
+     * @covers ::getDescription
+     * @uses \Foundry\Masonry\Core\Task::__construct
+     * @return void
+     */
+    public function testGetDescription()
+    {
+        $description = $this->getDescription();
+        $task = new Task($description);
+
+        $this->assertSame(
+            $description,
+            $task->getDescription()
+        );
+    }
+
+    /**
+     * @test
+     * @covers ::getStatus
+     * @uses \Foundry\Masonry\Core\Task::__construct
+     * @uses \Foundry\Masonry\Core\Task::getHistory
+     * @uses \Foundry\Masonry\Core\Task::start
+     * @uses \Foundry\Masonry\Core\Task::cancel
+     * @uses \Foundry\Masonry\Core\Task::complete
+     * @uses \Foundry\Masonry\Core\Task\History
+     * @uses \Foundry\Masonry\Core\Task\History\Reason
+     * @uses \Foundry\Masonry\Core\Task\History\Result
+     * @uses \Foundry\Masonry\Core\Task\History\Event
+     * @uses \Foundry\Masonry\Core\Task\Status
+     * @return void
+     */
+    public function testGetStatus()
+    {
+        $description = $this->getDescription();
+
+        $task1 = new Task($description);
+        $this->assertSame(
+            StatusInterface::STATUS_NEW,
+            (string)$task1->getStatus()
+        );
+
+        $task2 = new Task($description);
+        $task2->start();
+        $this->assertSame(
+            StatusInterface::STATUS_IN_PROGRESS,
+            (string)$task2->getStatus()
+        );
+
+        $task3 = new Task($description);
+        $task3->start();
+        $task3->cancel();
+        $this->assertSame(
+            StatusInterface::STATUS_DEFERRED,
+            (string)$task3->getStatus()
+        );
+
+        $task4 = new Task($description);
+        $task4->start();
+        $task4->complete();
+        $this->assertSame(
+            StatusInterface::STATUS_COMPLETE,
+            (string)$task4->getStatus()
+        );
+    }
+
+    /**
+     * @test
+     * @covers ::getHistory
+     * @uses \Foundry\Masonry\Core\Task::__construct
+     * @uses \Foundry\Masonry\Core\Task::getHistory
+     * @uses \Foundry\Masonry\Core\Task::start
+     * @uses \Foundry\Masonry\Core\Task\History
+     * @uses \Foundry\Masonry\Core\Task\History\Reason
+     * @uses \Foundry\Masonry\Core\Task\History\Result
+     * @uses \Foundry\Masonry\Core\Task\History\Event
+     * @return void
+     */
+    public function testGetHistory()
+    {
+        $description = $this->getDescription();
+        $task = new Task($description);
+
+        $this->assertInstanceOf(
+            '\Foundry\Masonry\Core\Task\History',
+            $task->getHistory()
+        );
+
+        $this->assertCount(
+            0,
+            $task->getHistory()->getEvents()
+        );
+
+        $task->start();
+
+        $this->assertCount(
+            1,
+            $task->getHistory()->getEvents()
+        );
+    }
+
+    /**
+     * @test
+     * @covers ::start
+     * @uses \Foundry\Masonry\Core\Task::__construct
+     * @uses \Foundry\Masonry\Core\Task::getHistory
+     * @uses \Foundry\Masonry\Core\Task::getStatus
+     * @uses \Foundry\Masonry\Core\Task\History
+     * @uses \Foundry\Masonry\Core\Task\History\Reason
+     * @uses \Foundry\Masonry\Core\Task\History\Result
+     * @uses \Foundry\Masonry\Core\Task\History\Event
+     * @uses \Foundry\Masonry\Core\Task\Status
+     * @return void
+     */
+    public function testStart()
+    {
+        $description = $this->getDescription();
+        $task = new Task($description);
+
+        $this->assertSame(
+            StatusInterface::STATUS_NEW,
+            (string)$task->getStatus()
+        );
+
+        $task->start();
+
+        $this->assertCount(
+            1,
+            $task->getHistory()->getEvents()
+        );
+
+        $this->assertSame(
+            StatusInterface::STATUS_IN_PROGRESS,
+            (string)$task->getStatus()
+        );
+    }
+
+    /**
+     * @test
+     * @covers ::complete
+     * @uses \Foundry\Masonry\Core\Task::__construct
+     * @uses \Foundry\Masonry\Core\Task::getHistory
+     * @uses \Foundry\Masonry\Core\Task::getStatus
+     * @uses \Foundry\Masonry\Core\Task::start
+     * @uses \Foundry\Masonry\Core\Task\Status
+     * @uses \Foundry\Masonry\Core\Task\History
+     * @uses \Foundry\Masonry\Core\Task\History\Reason
+     * @uses \Foundry\Masonry\Core\Task\History\Result
+     * @uses \Foundry\Masonry\Core\Task\History\Event
+     * @return void
+     */
+    public function testComplete()
+    {
+        $description = $this->getDescription();
+        $task = new Task($description);
+
+        $task->start();
+
+        $this->assertCount(
+            1,
+            $task->getHistory()->getEvents()
+        );
+
+        $task->complete();
+
+        $this->assertCount(
+            1,
+            $task->getHistory()->getEvents()
+        );
+
+        $this->assertSame(
+            StatusInterface::STATUS_COMPLETE,
+            (string)$task->getStatus()
+        );
+    }
+
+    /**
+     * @test
+     * @covers ::cancel
+     * @uses \Foundry\Masonry\Core\Task::__construct
+     * @uses \Foundry\Masonry\Core\Task::getHistory
+     * @uses \Foundry\Masonry\Core\Task::getStatus
+     * @uses \Foundry\Masonry\Core\Task::start
+     * @uses \Foundry\Masonry\Core\Task\Status
+     * @uses \Foundry\Masonry\Core\Task\History
+     * @uses \Foundry\Masonry\Core\Task\History\Reason
+     * @uses \Foundry\Masonry\Core\Task\History\Result
+     * @uses \Foundry\Masonry\Core\Task\History\Event
+     * @return void
+     */
+    public function testCancel()
+    {
+        $description = $this->getDescription();
+        $task = new Task($description);
+
+        $task->start();
+
+        $this->assertCount(
+            1,
+            $task->getHistory()->getEvents()
+        );
+
+        $task->cancel();
+
+        $this->assertCount(
+            1,
+            $task->getHistory()->getEvents()
+        );
+
+        $this->assertSame(
+            StatusInterface::STATUS_DEFERRED,
+            (string)$task->getStatus()
+        );
+    }
+}
