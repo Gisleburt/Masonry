37b566d03592774108a23df70bb61beecd96c198
Daniel Mason <daniel.mason@thefoundry.co.uk>
2016-01-07 13:13:24 +0000

    WorkerModule + Test

---
+++ src/Core/WorkerModule.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Module.php
+ * @author    Daniel Mason <daniel.mason@thefoundry.co.uk>
+ * @copyright 2015 The Foundry Visionmongers
+ * @see       https://github.com/TheFoundryVisionmongers/Masonry
+ */
+
+
+namespace Foundry\Masonry\Core;
+
+use Foundry\Masonry\Interfaces\Task\DescriptionInterface;
+use Foundry\Masonry\Interfaces\WorkerInterface;
+
+
+/**
+ * Class Module
+ * A module is a collection of Workers that can be imported in to any Masonry implementation to deal with tasks.
+ * @package Masonry
+ * @see       https://github.com/TheFoundryVisionmongers/Masonry
+ */
+class WorkerModule extends Mediator implements WorkerInterface
+{
+
+    /**
+     * @var DescriptionInterface[]
+     */
+    protected $descriptionTypes = [];
+
+    /**
+     * Module constructor.
+     * Workers can be initialised here.
+     * @param array $workers
+     */
+    public function __construct(array $workers)
+    {
+        foreach($workers as $worker) {
+            $this->addWorker($worker);
+        }
+    }
+
+    /**
+     * Lists, as strings, the class/interface names this module can handle.
+     * @see Foundry\Masonry\Interfaces\WorkerInterface::getDescriptionTypes
+     * @return DescriptionInterface[]
+     */
+    public function getDescriptionTypes()
+    {
+        if(!$this->descriptionTypes) {
+            foreach($this->workers as $worker) {
+                $workerDescriptionTypes = $worker->getDescriptionTypes();
+                foreach($workerDescriptionTypes as $descriptionType) {
+                    $this->descriptionTypes[$descriptionType] = $descriptionType;
+                }
+            }
+        }
+        return $this->descriptionTypes;
+    }
+
+}
