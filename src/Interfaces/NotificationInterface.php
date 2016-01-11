de31ef2097f98cc873793505a818ad0941003424
Daniel Mason <daniel.mason@thefoundry.co.uk>
2016-01-11 17:19:01 +0000

    Notifications moved from Masonry Builder

---
+++ src/Interfaces/NotificationInterface.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * NotificationInterface.php
+ * @author    Daniel Mason <daniel.mason@thefoundry.co.uk>
+ * @copyright 2015 The Foundry Visionmongers
+ * @license
+ * @see       https://github.com/TheFoundryVisionmongers/Masonry-Builder
+ */
+
+
+namespace Foundry\Masonry\Interfaces;
+
+/**
+ * Interface NotificationInterface
+ *
+ * @package Masonry-Builder
+ * @see       https://github.com/TheFoundryVisionmongers/Masonry-Builder
+ */
+interface NotificationInterface
+{
+
+    const PRIORITY_HIGH    = 0;
+    const PRIORITY_NORMAL  = 1;
+    const PRIORITY_INFO    = 2;
+    const PRIORITY_DEBUG   = 3;
+
+    /**
+     * The contents of the notification
+     * @return string
+     */
+    public function getMessage();
+
+    /**
+     * Get the importance of the notification.
+     * Lower numbers are better, 0 will always show.
+     * @return int
+     */
+    public function getPriority();
+
+}
