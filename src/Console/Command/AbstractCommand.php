670180ffd0a51a5bf210b314f6b3e84e21bc58bb
Daniel Mason <daniel.mason@thefoundry.co.uk>
2016-02-03 10:43:40 +0000

    Moved traits to abstract class to simplify further.

--- src/Console/Command/AbstractCommand.php
+++ src/Console/Command/AbstractCommand.php
@@ -9,10 +9,13 @@
 
 namespace Foundry\Masonry\Console\Command;
 
-use Foundry\Masonry\Console\Command\Shared\LoggerTrait;
-use Foundry\Masonry\Console\Command\Shared\QueueTrait;
 use Foundry\Masonry\Core\Injection\HasFilesystem;
+use Foundry\Masonry\Logging\MultiLogger;
+use Foundry\Masonry\Logging\SymfonyOutputLogger;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Class AbstractCommand
@@ -23,11 +26,20 @@ use Symfony\Component\Console\Command\Command;
 abstract class AbstractCommand extends Command
 {
 
-    use QueueTrait;
-    use LoggerTrait;
     use HasFilesystem;
 
     /**
+     * @var string
+     */
+    protected $defaultFileName = 'queue.yaml';
+
+    /**
+     * @var string
+     */
+    protected $configOptionName = 'queue';
+
+    /**
+     * Sets up the command
      * @param string $name
      * @param string $description
      */
@@ -42,5 +54,53 @@ abstract class AbstractCommand extends Command
         $this->getNativeDefinition()->addArgument(
             $this->getQueueArgument()
         );
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @return MultiLogger
+     */
+    protected function setUpLogger(OutputInterface $output)
+    {
+        $logger = new MultiLogger();
+        $logger->addLogger(
+            new SymfonyOutputLogger($output)
+        );
+        return $logger;
+    }
+
+    /**
+     * @return InputArgument
+     */
+    protected function getQueueArgument()
+    {
+        return new InputArgument(
+            $this->configOptionName,
+            InputArgument::OPTIONAL,
+            'The name of the initial queue to use',
+            $this->defaultFileName
+        );
+    }
+
+    /**
+     * @param InputInterface|null $input
+     * @return string
+     */
+    protected function getQueueFullPath(InputInterface $input = null)
+    {
+        if ($input && $input->hasArgument($this->configOptionName)) {
+            return $input->getArgument($this->configOptionName);
+        }
+        return $this->defaultFileName;
+    }
+
+    /**
+     * Gets the current working directory
+     * Just a wrapper in case we need to do something more complex
+     * @return string
+     */
+    protected function getCwd()
+    {
+        return getcwd();
     }
 }
