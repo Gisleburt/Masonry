e12ca7807836518744a40c1b6922d6d2598c5475
Daniel Mason <daniel.mason@thefoundry.co.uk>
2016-02-03 13:40:18 +0000

    Unused use

--- src/Core/Module/WorkerModule.php
+++ src/Core/Module/WorkerModule.php
@@ -11,7 +11,6 @@ namespace Foundry\Masonry\Core\Module;
 
 use Foundry\Masonry\Core\Mediator;
 use Foundry\Masonry\Interfaces\Task\DescriptionInterface;
-use Foundry\Masonry\Interfaces\WorkerInterface;
 use Foundry\Masonry\Interfaces\WorkerModuleInterface;
 
 /**
