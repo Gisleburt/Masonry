49d124f75d989e521f8852ac8489a642d8ba9ea1
Daniel Mason <daniel.mason@thefoundry.co.uk>
2016-02-04 12:42:07 +0000

    Fix bug in Abstract Group Description uncovered in tests

--- src/Workers/Group/AbstractGroupDescription.php
+++ src/Workers/Group/AbstractGroupDescription.php
@@ -45,7 +45,8 @@ abstract class AbstractGroupDescription extends AbstractDescription implements P
      */
     public function addTask(TaskInterface $task)
     {
-        return $this->pool->addTask($task);
+        $this->pool->addTask($task);
+        return $this;
     }
 
     /**
