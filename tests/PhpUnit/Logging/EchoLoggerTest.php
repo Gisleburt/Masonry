ce200ad0a6525ca8129d8dd52d1475e4f8872304
Daniel Mason <daniel.mason@thefoundry.co.uk>
2016-02-05 18:17:19 +0000

    Echo Logger Test

--- tests/PhpUnit/Logging/EchoLoggerTest.php
+++ tests/PhpUnit/Logging/EchoLoggerTest.php
@@ -27,4 +27,33 @@ class EchoLoggerTest extends AbstractSimpleLoggerTest
     {
         return EchoLogger::class;
     }
+
+    /**
+     * @test
+     * @covers ::log
+     */
+    public function testLog()
+    {
+        $logLevel = 'test level';
+        $logMessage = 'test message';
+        $logContext = [];
+
+        $testString = 'test string';
+
+        /** @var EchoLogger|\PHPUnit_Framework_MockObject_MockObject $logger */
+        $logger =
+            $this
+                ->getMockBuilder($this->getTestSubjectClass())
+                ->setMethods(['formatLog'])
+                ->getMock();
+        $logger
+            ->expects($this->once())
+            ->method('formatLog')
+            ->with($logLevel, $logMessage)
+            ->will($this->returnValue($testString));
+
+        $this->expectOutputString($testString . PHP_EOL);
+
+        $logger->log($logLevel, $logMessage, $logContext);
+    }
 }
