4341ea230c9e84c0bb8cade6472c6060bc56589b
Daniel Mason <daniel.mason@thefoundry.co.uk>
2016-02-10 11:47:01 +0000

    Finish Init tests

--- tests/PhpUnit/Console/InitTest.php
+++ tests/PhpUnit/Console/InitTest.php
@@ -10,7 +10,11 @@
 namespace Foundry\Masonry\Tests\PhpUnit\Console;
 
 use Foundry\Masonry\Console\Command\Init;
-use Symfony\Component\Console\Input\InputArgument;
+use Foundry\Masonry\Core\GlobalRegister;
+use Foundry\Masonry\Logging\MultiLogger;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Class InitTest
@@ -20,6 +24,26 @@ use Symfony\Component\Console\Input\InputArgument;
  */
 class InitTest extends AbstractCommandTest
 {
+    private $oldLogger;
+
+    /**
+     * Some tests change states so record them before the test here.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->oldLogger = $this->getStaticAttribute(GlobalRegister::class, 'logger');
+    }
+
+    /**
+     * Put the system back into it's old state.
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->setStaticAttribute(GlobalRegister::class, 'logger', $this->oldLogger);
+    }
+
     /**
      * @return string
      */
@@ -46,5 +70,123 @@ class InitTest extends AbstractCommandTest
             'Initialise Masonry in the current directory with a masonry.yaml',
             $command->getDescription()
         );
+    }
+
+    /**
+     * @test
+     * @covers ::execute
+     * @uses \Foundry\Masonry\Console\Command\AbstractCommand::getQueueArgument
+     * @uses \Foundry\Masonry\Console\Command\AbstractCommand::abstractConfigure
+     * @uses \Foundry\Masonry\Core\GlobalRegister
+     */
+    public function testExecute()
+    {
+        $testFileName = 'test-file-name';
+        $input  = $this->getMockForAbstractClass(InputInterface::class);
+        $output = $this->getMockForAbstractClass(OutputInterface::class);
+
+        $logger = $this->getMock(MultiLogger::class);
+        $logger
+            ->expects($this->at(0))
+            ->method('info')
+            ->with($this->stringContains("Creating"));
+        $logger
+            ->expects($this->at(1))
+            ->method('info')
+            ->with("Done");
+
+        $fileSystem = $this->getMock(Filesystem::class);
+        $fileSystem
+            ->expects($this->once())
+            ->method('exists')
+            ->with($testFileName)
+            ->will($this->returnValue(false));
+        $fileSystem
+            ->expects($this->once())
+            ->method('dumpFile')
+            ->with($testFileName, '');
+
+        $init =
+            $this
+                ->getMockBuilder(Init::class)
+                ->disableOriginalConstructor()
+                ->setMethods(['setUpLogger', 'getFilesystem', 'getQueueFullPath',])
+                ->getMock();
+        $init
+            ->expects($this->once())
+            ->method('setUpLogger')
+            ->with($output)
+            ->will($this->returnValue($logger));
+        $init
+            ->expects($this->once())
+            ->method('getFilesystem')
+            ->with()
+            ->will($this->returnValue($fileSystem));
+        $init
+            ->expects($this->once())
+            ->method('getQueueFullPath')
+            ->with($input)
+            ->will($this->returnValue($testFileName));
+
+        $execute = $this->getObjectMethod($init, 'execute');
+
+        $execute($input, $output);
+    }
+
+    /**
+     * @test
+     * @covers ::execute
+     * @uses \Foundry\Masonry\Console\Command\AbstractCommand::getQueueArgument
+     * @uses \Foundry\Masonry\Console\Command\AbstractCommand::abstractConfigure
+     * @uses \Foundry\Masonry\Core\GlobalRegister
+     * @expectedException \Foundry\Masonry\Console\Exception\FileExistsException
+     * @expectedExceptionMessage File 'test-file-name' already exists
+     */
+    public function testExecuteException()
+    {
+        $testFileName = 'test-file-name';
+        $input  = $this->getMockForAbstractClass(InputInterface::class);
+        $output = $this->getMockForAbstractClass(OutputInterface::class);
+
+        $logger = $this->getMock(MultiLogger::class);
+        $logger
+            ->expects($this->never())
+            ->method('info');
+
+        $fileSystem = $this->getMock(Filesystem::class);
+        $fileSystem
+            ->expects($this->once())
+            ->method('exists')
+            ->with($testFileName)
+            ->will($this->returnValue(true));
+        $fileSystem
+            ->expects($this->never())
+            ->method('dumpFile');
+
+        $init =
+            $this
+                ->getMockBuilder(Init::class)
+                ->disableOriginalConstructor()
+                ->setMethods(['setUpLogger', 'getFilesystem', 'getQueueFullPath',])
+                ->getMock();
+        $init
+            ->expects($this->once())
+            ->method('setUpLogger')
+            ->with($output)
+            ->will($this->returnValue($logger));
+        $init
+            ->expects($this->once())
+            ->method('getFilesystem')
+            ->with()
+            ->will($this->returnValue($fileSystem));
+        $init
+            ->expects($this->once())
+            ->method('getQueueFullPath')
+            ->with($input)
+            ->will($this->returnValue($testFileName));
+
+        $execute = $this->getObjectMethod($init, 'execute');
+
+        $execute($input, $output);
     }
 }
